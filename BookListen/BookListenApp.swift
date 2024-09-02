d492bd85e8a7e68b42e4fee61120e239dfee3103
Ira Tretiakova <inikolenko@jamworks.com>
2024-09-02 17:39:15 +0300

    initial commit

---
+++ BookListen/BookListenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookListenApp.swift
+//  BookListen
+//
+//  Created by Ira Tretiakova on 02.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookListenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
